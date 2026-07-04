cfe9aed5f2a2f05d58c358cb7d3051ef455f7e00
xunian3365 <3365262779@qq.com>
2026-07-04 08:34:59 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := zygisk
 LOCAL_SRC_FILES := zygisk_main.cpp
-LOCAL_LDFLAGS += -llog -ldl
-LOCAL_CFLAGS += -std=c++17 -Os
+LOCAL_CPPFLAGS += -std=c++17 -Wall -Wextra
+LOCAL_LDLIBS += -llog -ldl
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+
 include $(BUILD_SHARED_LIBRARY)
